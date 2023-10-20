1d00be6698e7082b8e9da61a19c39f83bde88d73
jacques <zedigames4@gmail.com>
2023-10-20 16:23:11 +0300

    adding first commit

---
+++ Downloads/math/HTML5 Games/Addition Portrait/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1513001532,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/timer-sheet0.png",
+		"images/how-sheet0.png",
+		"images/start_box-sheet0.png",
+		"images/title_box-sheet0.png",
+		"images/start-sheet0.png",
+		"images/title-sheet0.png",
+		"images/q_box-sheet0.png",
+		"images/menu-sheet0.png",
+		"images/score_text-sheet0.png",
+		"images/equal_sign-sheet0.png",
+		"images/plus_sign-sheet0.png",
+		"media/correct.m4a",
+		"media/correct.ogg",
+		"media/wrong.m4a",
+		"media/wrong.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
